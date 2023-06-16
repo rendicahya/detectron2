eb09cead660a0d51450a6fbf5d81794b4c8dbc68
Randy Cahya Wihandika <rendicahya@gmail.com>
2023-06-16 18:04:41 +0900

    Formatting

--- batch.py
+++ batch.py
@@ -38,7 +38,9 @@ def get_parser():
     parser = argparse.ArgumentParser(description="Detectron2 demo for builtin configs")
 
     parser.add_argument("input", type=str, help="The video input directory.", nargs="?")
-    parser.add_argument("output", type=str, help="The video output directory.", nargs="?")
+    parser.add_argument(
+        "output", type=str, help="The video output directory.", nargs="?"
+    )
     parser.add_argument(
         "--config-file",
         default="configs/quick_schedules/mask_rcnn_R_50_FPN_inference_acc_test.yaml",
